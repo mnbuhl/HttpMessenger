152298d52936ba73bde5a32396e1052f6e105590
Mikkel Buhl <mikkelnbuhl@gmail.com>
2022-01-30 12:45:23 +0100

    Added documentation to the HttpService

--- src/HttpService/Helpers/ResponseWrapper.cs
+++ src/HttpService/Helpers/ResponseWrapper.cs
@@ -2,8 +2,20 @@
 
 namespace HttpService.Helpers;
 
-public record ResponseWrapper<T>(bool Success, T Data, HttpStatusCode StatusCode, string? ErrorMessage = null)
-{
-    
-};
-public record ResponseWrapper(bool Success, HttpStatusCode StatusCode, string? ErrorMessage = null);
+/// <summary>
+/// A generic easy to use helper class for wrapping the content from a HTTP response.
+/// </summary>
+/// <param name="Success">The success status of the response</param>
+/// <param name="Data">The data returned from the response</param>
+/// <param name="StatusCode">The status code of the response</param>
+/// <param name="ErrorMessage">Optional error message - used when the request fails</param>
+/// <typeparam name="T">The generic data model returned from the response</typeparam>
+public record ResponseWrapper<T>(bool Success, T Data, int StatusCode, string? ErrorMessage = null);
+
+/// <summary>
+/// An easy to use helper class for wrapping the content from a HTTP response.
+/// </summary>
+/// <param name="Success">The success status of the response</param>
+/// <param name="StatusCode">The status code of the response</param>
+/// <param name="ErrorMessage">Optional error message - used when the request fails</param>
+public record ResponseWrapper(bool Success, int StatusCode, string? ErrorMessage = null);
--- src/HttpService/RegisterHttpServiceExtension.cs
+++ src/HttpService/RegisterHttpServiceExtension.cs
@@ -5,6 +5,11 @@ namespace HttpService
 {
     public static class RegisterHttpServiceExtension
     {
+        /// <summary>
+        /// Registers the HttpService to the services container.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
         public static IServiceCollection AddHttpService(this IServiceCollection services)
         {
             services.AddScoped<IHttpService, Service.HttpService>();
